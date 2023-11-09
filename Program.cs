a018a41905869438a5e3ad0c489fb2db7ebe9d32
Krystofek2007 <kriss.prokopgmail.com>
2023-11-09 09:54:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*using System.Reflection.Metadata.Ecma335;
+
+int input1 = 5;
+
+
+
+if (input1 < 0)
+{
+    Console.WriteLine(input1);
+}
+else
+{
+    Console.WriteLine(input1 * -1);
+}
+
+
+int result = 0;
+if (input1 <= 0)
+{
+    result = input1;
+}
+else
+{
+    result = input1 * -1;
+}
+*/
+
+string name = "Kryštof";
+string Great = "Hello, ";
+string GreatEnd = " how are you doing today?";
+public static string Greet(string name)
+{
+    throw new NotImplementedException("Greet is not implemented.");
+}
